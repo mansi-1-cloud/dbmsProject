4f8d0db02e099778741734635b899b83ced0abdd
Garv <garvbarthwal@gmail.com>
2025-11-10 11:05:02 +0530

    removed errors in frontend#2

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -262,7 +262,7 @@ function ExpandableCardDemo() {
         ) : null}
       </AnimatePresence>
       <ul className="max-w-2xl mx-auto w-full gap-4">
-        {cards.map((card, index) => (
+        {cards.map((card) => (
           <motion.div
             layoutId={`card-${card.title}-${id}`}
             key={`card-${card.title}-${id}`}
--- frontend/src/pages/UserDashboard.tsx
+++ frontend/src/pages/UserDashboard.tsx
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
 import {
   IconArrowLeft,
-  IconBrandTabler,
   IconUser,
   IconClipboardList,
   IconClock,
